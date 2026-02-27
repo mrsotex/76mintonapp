da7b9ff8763a3dbb9b197278f07237fa2fc54822
조우택 <utis@utisone.co.kr>
2026-02-27 20:40:13 +0900

    로그인 css 기능 수정

--- js/login.js
+++ js/login.js
@@ -8,13 +8,13 @@ function togglePassword() {
     const eyeIcon = document.getElementById('eyeIcon');
 
     if (passwordVisible) {
-        pwInput.type = 'text';
+        pwInput.style.webkitTextSecurity = 'none';
         eyeIcon.innerHTML = `
             <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
             <circle cx="12" cy="12" r="3"></circle>
         `;
     } else {
-        pwInput.type = 'password';
+        pwInput.style.webkitTextSecurity = 'disc';
         eyeIcon.innerHTML = `
             <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94"></path>
             <path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19"></path>
